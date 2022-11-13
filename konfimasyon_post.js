1284df59139186bd22cc97f9802b549056505e4c
Jameson Berendoive <gerardinho105@gmail.com>
2022-11-12 21:35:41 -0400

    first commit

---
+++ konfimasyon_post.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function getConfirmation() {
+  var retVal = confirm("Eske w vle pataje kontni sa pou 24 edtan?");
+  //   if (retVal == true) {
+  //     document.write("User wants to continue!");
+  //     return true;
+  //   } else {
+  //     document.write("User does not want to continue!");
+  //     return false;
+  //   }
+}
